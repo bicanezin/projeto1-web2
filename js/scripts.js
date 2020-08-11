793cb22340414977fffd0032a9f9f5606cacf5e3
Bianca Canezin <bcanezin@alunos.utfpr.edu.br>
2020-08-11 10:33:48 -0300

    atualiza funções copiar link e ver detalhes

--- js/scripts.js
+++ js/scripts.js
@@ -26,50 +26,38 @@ const fillTableWithAllLinks = ({ data }) => {
 
   const newLine =
     "<tr>" +
-    `<td><a id="shortened_link" href=${newUrl} target="_blank">${newUrl}</a>` +
-    `<td><img src="images/copy.png" id="btn_copy" width="23" height="23"> ` +
-    `<td><img src="images/see_more.png" id="btn_see_more" width="23" height="23"> ` +
+    `<td><a id="${newUrl}" class="shortened_link" href=${newUrl} target="_blank">${newUrl}</a>` +
+    `<td><img onclick="btnCopyOnClick('${hashid}')" src="images/copy.png" id="btn_copy" width="23" height="23"> ` +
+    `<td><img onclick="btnSeeMoreOnClick('${hashid}')" src="images/see_more.png" id="btn_see_more" width="23" height="23"> ` +
     "</tr>";
   $("#url").val("");
   $(".table-primary > tbody > tr:last").after(newLine);
-
-  const hashID = newUrl.split("/")[3];
-
-  $("#btn_copy").click(() => {
-    btnCopyOnClick({ element: "shortened_link" });
-  });
-
-  $("#btn_see_more").click(() => {
-    btnSeeMoreOnClick({ hashid: hashID });
-  });
 };
 
 const fillTableWithLinkDetails = ({ data }) => {
   const newLine =
     "<tr>" +
     `<td><a id="original_link" href=${data.url} target="_blank">${data.url}</a>` +
-    `<td><img src="images/copy.png" id="btn_copy" width="23" height="23"> ` +
     `<td><a>${new Date(data.created_at).toLocaleDateString()}</a>` +
     "</tr>";
   $(".table-details > tbody > tr:last").empty();
   $(".table-details > tbody > tr:last").after(newLine);
-
-  $("#btn_copy").click(() => {
-    btnCopyOnClick({ element: "original_link" });
-  });
 };
 
-const btnCopyOnClick = ({ element }) => {
-  const link = document.getElementById(element);
+const btnCopyOnClick = (hashId) => {
+  newUrl = `https://rel.ink/${hashId}`;
+
+  const link = document.getElementById(newUrl);
   const range = document.createRange();
   range.selectNode(link);
   const selection = window.getSelection();
   selection.removeAllRanges();
   selection.addRange(range);
-  document.execCommand("copy");
+
+  const successful = document.execCommand("copy");
 };
 
-const btnSeeMoreOnClick = ({ hashid }) => {
+const btnSeeMoreOnClick = (hashid) => {
   $.ajax({
     method: "GET",
     url: `https://rel.ink/api/links/${hashid}`,
